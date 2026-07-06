d7f68d532db9b1da2b2b7a8978c76bbfb3446a18
Manar-sami <dweikatmanar@gmail.com>
2026-07-06 22:02:32 +0300

    add getproducts page

---
+++ src/Hook/Getproducts.jsx
@@ -0,0 +1,26 @@
+
+import axiosinstance from './../API/axiosinstance'
+import { useQuery } from "@tanstack/react-query";
+
+function Getproducts() {
+
+  const getproducts=async()=>{
+    const response=await axiosinstance.get("/products");
+    console.log(response.data)
+    return response.data; 
+    
+  }  
+
+  const query =useQuery({
+    queryKey: ['products'],
+    queryFn: getproducts,
+    staleTime: 1000*60*5,
+  })
+  
+
+
+
+  return query;
+}
+
+export default Getproducts
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,7 +1,39 @@
-
+import useGetproducts from "../../Hook/Getproducts";
 function Products() {
+  
+   
+  const {data, isLoading, error} = useGetproducts();
+  if (isLoading) {
+    return (
+      <>
+        <h2>Loading...</h2>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <h2>Error</h2>
+      </>
+    );
+  
+  }
+  console.log(data.response.data)
   return (
-    <div>Products</div>
+    <>
+    <h2 className="text-2xl font-bold mb-4">Products</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 container mx-auto py-20">
+        {data.response.data.map((product) => (
+      <div className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300" key={product.id}>
+        <img src={product.image} alt={product.name}  className="w-full h-[400px]"/>
+        <h3 className="text-lg font-bold">{product.name}</h3>
+        <span className="text-gray-600">{product.price}</span>
+        
+      </div>
+    ))}
+      </div>
+    </>
   )
 }
 
